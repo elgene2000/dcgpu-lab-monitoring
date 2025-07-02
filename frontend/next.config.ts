c4c5316f685f0f37627fa3a85514802a8bcc4c20
kangchin <kangchin@amd.com>
2025-07-02 09:28:59 +0800

    Update next.config.ts
    added code so that /api is automatically removed when the system is run on local

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/:path*", // Proxy to backend in dev
+      },
+    ];
+  },
 };
 
 export default nextConfig;
