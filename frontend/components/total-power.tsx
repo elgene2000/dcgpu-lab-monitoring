3644270bd2ac3f563a9850251d82c358f00be81e
Chin <kangchin@amd.com>
2025-08-20 14:33:19 +0800

    add back total power card testing

--- frontend/components/total-power.tsx
+++ frontend/components/total-power.tsx
@@ -8,6 +8,8 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+
+
 const totalPowerAvailability: Record<
   "odcdh1" | "odcdh2" | "odcdh3" | "odcdh4" | "odcdh5",
   number
@@ -30,45 +32,29 @@ const TotalPowerCard = ({
   index,
   totalPower,
   theme,
-  datahall,
-}: TotalPowerCardProps) => {
-  const router = useRouter();
-
+}: {
+  index: number;
+  totalPower: number;
+  theme?: string;
+}) => {
   return (
-    <div
-      onClick={() => router.push(`/opendc/dh${index}/total-power`)}
-      className="transform hover:cursor-pointer hover:scale-[1.03] overflow-visible transition-transform duration-500 ease-in-out"
-    >
-      <Card className="w-full p-2">
-        <CardHeader className="text-left ">
-          <CardTitle className="flex items-center">
-            <DoorOpen
-              className="dark:bg-[#363E50] bg-[#E9EDEF] p-1 rounded-md h-7 w-7"
-              color={theme == "dark" ? "#7B8499" : "#A8ABBE"}
-            />
-            <span className="ml-2">Data Hall {index}</span>
-          </CardTitle>
-          <div className="mt-4 pt-4">
-            <Tooltip>
-              <TooltipTrigger className="cursor-pointer" asChild>
-                <Progress
-                  value={(totalPower / totalPowerAvailability[datahall]) * 100}
-                />
-              </TooltipTrigger>
-              <TooltipContent className="dark:bg-secondary-dark dark:border-none dark:text-white bg-white border text-black">
-                <p className="font-semibold">
-                  {(totalPower / 1000).toFixed(2)} kW
-                </p>
-              </TooltipContent>
-              <p className="text-right font-light text-xs">
-                {totalPowerAvailability[datahall] / 1000} kW
-              </p>
-            </Tooltip>
-          </div>
-        </CardHeader>
-      </Card>
-    </div>
+    <Card className="w-full p-2">
+      <CardHeader className="text-left ">
+        <CardTitle className="flex items-center">
+          <DoorOpen
+            className="dark:bg-[#363E50] bg-[#E9EDEF] p-1 rounded-md h-7 w-7"
+            color={theme == "dark" ? "#7B8499" : "#A8ABBE"}
+          />
+          <span className="ml-2">Data Hall {index}</span>
+        </CardTitle>
+        <p className="text-2xl font-bold pt-4">
+          {(totalPower / 1000).toFixed(2)} kW
+        </p>
+        <p className="text-xs font-light">Current month</p>
+      </CardHeader>
+    </Card>
   );
 };
+
 
 export { TotalPowerCard };
